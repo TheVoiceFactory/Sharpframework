0e6067fb5a402784cb4100660bf91c843c0463a1
Roberto Duc <roberto.duc@gmail.com>
2019-05-16 15:48:25 +0200

    Waving con Roslyn

--- src/Test.DependencyInjection/Program.cs
+++ src/Test.DependencyInjection/Program.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading;
 using System.Threading.Tasks;
 
+
+namespace Test.Attributes
+{
+    public class MethodsEpilogAttribute : Attribute
+    {
+        public MethodsEpilogAttribute ( String epilogMethodName ) { }
+    }
+
+    public class MethodsPrologAttribute : Attribute
+    {
+        public MethodsPrologAttribute ( String prologMethodName ) { }
+    }
+}
 
 namespace Test.DependencyInjection
 {
@@ -72,9 +89,256 @@ namespace Test.DependencyInjection
             }
         }
 
+
+        //[ MethodsEpilog ( nameof ( Program1._Epilog ) ) ]
+        //public class Program1
+        //{
+        //    static void Main ( string [] args )
+        //    {
+        //        object s = "abc";
+        //    }
+
+        //    public void Pippo ()
+        //    {
+        //    }
+
+        //    private void _Epilog ()
+        //    {
+        //    }
+
+        //    private static void _Prolog ()
+        //    {
+        //    }
+        //}
+        public static class RsolynTest
+        {
+            private const string ProgramString = @"
+                using System;
+                using Test.Attributes;
+
+                namespace MakeConstTest
+                {
+                    [ MethodsProlog ( ""_Prolog"" ) ]
+                    [ MethodsEpilog ( ""_Epilog"" ) ]
+                    public class Program
+                    {
+                        static void Main ( string [] args )
+                        {
+                            object s = ""abc"";
+                            Int32  k = 12;
+
+                            for ( Int32 k = 0 ; k < 10 ; k++ )
+                            {
+                                s += k.ToString ();
+                            }
+                        }
+
+                        public void Pippo ()
+                        {
+                        }
+
+                        private static void _Epilog ()
+                        {
+                        }
+
+                        private static void _Prolog ()
+                        {
+                        }
+                    }
+                }";
+         
+            private class PrologEpilogSyntaxVisitor
+                : CSharpSyntaxVisitor<InvocationExpressionSyntax>
+            {
+                public override InvocationExpressionSyntax VisitClassDeclaration ( ClassDeclarationSyntax node )
+                {
+                    return base.VisitClassDeclaration ( node );
+                }
+                public override InvocationExpressionSyntax VisitMethodDeclaration ( MethodDeclarationSyntax node )
+                {
+                    return base.VisitMethodDeclaration ( node );
+                }
+                public override InvocationExpressionSyntax VisitAttribute ( AttributeSyntax node )
+                {
+                    return base.VisitAttribute ( node );
+                }
+            }
+            private class MethodPrologEpilogRewriter : CSharpSyntaxRewriter
+            {
+                public override SyntaxNode VisitBlock ( BlockSyntax node )
+                {
+                    SyntaxNode retVal = base.VisitBlock ( node );
+
+                    InvocationExpressionSyntax epilogInvocation =
+                        SyntaxFactory.InvocationExpression (
+                            SyntaxFactory.IdentifierName ( "_Epilog" ) );
+
+                    InvocationExpressionSyntax prologInvocation =
+                        SyntaxFactory.InvocationExpression (
+                            SyntaxFactory.IdentifierName ( "_Prolog" ) );
+
+                    ExpressionStatementSyntax prologInvocationStatement =
+                        SyntaxFactory.ExpressionStatement ( prologInvocation );
+
+                    ExpressionStatementSyntax epilogInvocationStatement =
+                        SyntaxFactory.ExpressionStatement ( epilogInvocation );
+
+                    BlockSyntax block = node.AddStatements ( epilogInvocationStatement );
+
+                    block = block.InsertNodesBefore (
+                                    block.Statements [ 0 ],
+                                    new SyntaxNode [] { prologInvocationStatement } );
+
+                    return block;
+                }
+            }
+            private class ClassRewriter : CSharpSyntaxRewriter
+            {
+                private MethodPrologEpilogRewriter __methodBodyrewriter;
+
+                private MethodPrologEpilogRewriter _MethodBodyRewriter
+                {
+                    get
+                    {
+                        if ( __methodBodyrewriter == null )
+                            __methodBodyrewriter = new MethodPrologEpilogRewriter ();
+
+                        return __methodBodyrewriter;
+                    }
+                }
+
+                public override SyntaxNode VisitAttribute ( AttributeSyntax node )
+                {
+                    String _GetAttributeName ()
+                    {
+                        IdentifierNameSyntax idName;
+
+                        if ( (idName = node.Name as IdentifierNameSyntax) != null )
+                            return idName.Identifier.ValueText;
+
+                        return String.Empty;
+                    }
+
+                    String _MethodPrologName ( ExpressionSyntax expr )
+                    {
+                        LiteralExpressionSyntax litExpr;
+
+                        if ( (litExpr = expr as LiteralExpressionSyntax) != null )
+                            return litExpr.Token.ValueText;
+
+                        return String.Empty;
+                    }
+
+                    if ( node.ArgumentList == null )
+                        return base.VisitAttribute ( node );
+                    
+                    if ( node.ArgumentList.Arguments== null )
+                        return base.VisitAttribute ( node );
+
+                    if ( node.ArgumentList.Arguments.Count != 1)
+                        return base.VisitAttribute ( node );
+
+                    String methodPrologName = String.Empty;
+
+                    if ( _GetAttributeName () == "MethodsProlog" )
+                        methodPrologName = _MethodPrologName ( node.ArgumentList.Arguments [0].Expression );
+                        //IdentifierNameSyntax idName = node.Name as IdentifierNameSyntax;
+
+                        //if ( idName != null && idName.Identifier.ValueText == "MethodsProlog" )
+                        //    ;
+
+                        //if ( node.Name == "Pippo" ) return node;
+
+                        return base.VisitAttribute ( node );
+                }
+                public override SyntaxNode VisitBlock ( BlockSyntax node )
+                {
+                    if ( node.Parent is MethodDeclarationSyntax )
+                        if ( node.Statements.Count > 0 )
+                            return base.VisitBlock ( node );
+                        else
+                            return node.AddStatements ( _PrologInvocationStatement,
+                                                        _EpilogInvocationStatement );
+                    else
+                        return node;
+                }
+                public override SyntaxList<TNode> VisitList<TNode> ( SyntaxList<TNode> list )
+                {
+                    if ( list.Count < 1 )
+                        return base.VisitList ( list );
+
+                    if ( !(list [0].Parent is BlockSyntax) )
+                        return base.VisitList ( list );
+
+                    if ( !(list [0].Parent.Parent is MethodDeclarationSyntax) )
+                        return base.VisitList ( list );
+
+                    return list
+                            .Insert ( 0, _PrologInvocationStatement as TNode )
+                            .Add ( _EpilogInvocationStatement as TNode );
+                    //return base.VisitList ( list );
+                }
+                public override SyntaxNode VisitMethodDeclaration ( MethodDeclarationSyntax node )
+                {
+                    if ( node.Identifier.ValueText == "Main" )
+                        return base.VisitMethodDeclaration ( node );
+
+                    if ( node.Identifier.ValueText == "Pippo" )
+                        return base.VisitMethodDeclaration ( node );
+
+                    return node;
+                    //SyntaxNode retVal = base.VisitMethodDeclaration ( node );
+
+                    //if ( node.Identifier.ValueText == "Main" || node.Identifier.ValueText == "Pippo" )
+                    //{
+                    //    SyntaxNode rewrittenMethod = _MethodBodyRewriter.Visit ( node );
+
+                    //    return rewrittenMethod == null ? node : rewrittenMethod;
+                    //}
+
+                    //return retVal;
+                }
+
+
+                private InvocationExpressionSyntax _EpilogInvocationSyntax
+                {
+                    get => SyntaxFactory.InvocationExpression (
+                            SyntaxFactory.IdentifierName ( "_Epilog" ) );
+                }
+
+                private ExpressionStatementSyntax _EpilogInvocationStatement
+                { get => SyntaxFactory.ExpressionStatement ( _EpilogInvocationSyntax ); }
+
+                private InvocationExpressionSyntax _ProogInvocationSyntax
+                {
+                    get => SyntaxFactory.InvocationExpression (
+                            SyntaxFactory.IdentifierName ( "_Prolog" ) );
+                }
+
+                private ExpressionStatementSyntax _PrologInvocationStatement
+                { get => SyntaxFactory.ExpressionStatement ( _ProogInvocationSyntax ); }
+
+            }
+            public static void Pippo ()
+            {
+                MSBuildWorkspace workspace = MSBuildWorkspace.Create ();
+                ClassRewriter   classRwrt;
+                SyntaxTree      synTree;
+                SyntaxNode      rewritten;
+
+                classRwrt   = new ClassRewriter ();
+                synTree     = CSharpSyntaxTree.ParseText ( ProgramString );
+                rewritten   = classRwrt.Visit ( synTree.GetRoot () );
+
+                Console.WriteLine ( rewritten.NormalizeWhitespace ().ToFullString () );
+                //CSharpCompilation csc = new CSharpCompilation (
+            }
+        }
         static async Task Main ( string [] args )
         {
-            IHostBuilder hb = new HostBuilder ()
+            RsolynTest.Pippo ();
+
+             IHostBuilder hb = new HostBuilder ()
                 .ConfigureServices((hostContext, services) =>
                 {
                     //services.AddHostedService<MyHostedService> ();
---
+++ src/Test.Roslyn/PrologEpilogRewriter.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+
+namespace Test.Roslyn
+{
+    public class PrologEpilogRewriter : CSharpSyntaxRewriter
+    {
+            public override SyntaxNode VisitAttribute ( AttributeSyntax node )
+            {
+                String _GetAttributeName ()
+                {
+                    IdentifierNameSyntax idName;
+
+                    if ( (idName = node.Name as IdentifierNameSyntax) != null )
+                        return idName.Identifier.ValueText;
+
+                    return String.Empty;
+                }
+
+                String _MethodPrologName ( ExpressionSyntax expr )
+                {
+                    LiteralExpressionSyntax litExpr;
+
+                    if ( (litExpr = expr as LiteralExpressionSyntax) != null )
+                        return litExpr.Token.ValueText;
+
+                    return String.Empty;
+                }
+
+                if ( node.ArgumentList == null )
+                    return base.VisitAttribute ( node );
+                    
+                if ( node.ArgumentList.Arguments== null )
+                    return base.VisitAttribute ( node );
+
+                if ( node.ArgumentList.Arguments.Count != 1)
+                    return base.VisitAttribute ( node );
+
+                String methodPrologName = String.Empty;
+
+                if ( _GetAttributeName () == "MethodsProlog" )
+                    methodPrologName = _MethodPrologName ( node.ArgumentList.Arguments [0].Expression );
+
+                return base.VisitAttribute ( node );
+            }
+            public override SyntaxNode VisitBlock ( BlockSyntax node )
+            {
+                if ( node.Parent is MethodDeclarationSyntax )
+                    if ( node.Statements.Count > 0 )
+                        return base.VisitBlock ( node );
+                    else
+                        return node.AddStatements ( _PrologInvocationStatement,
+                                                    _EpilogInvocationStatement );
+                else
+                    return node;
+            }
+            public override SyntaxList<TNode> VisitList<TNode> ( SyntaxList<TNode> list )
+            {
+                if ( list.Count < 1 )
+                    return base.VisitList ( list );
+
+                if ( !(list [0].Parent is BlockSyntax) )
+                    return base.VisitList ( list );
+
+                if ( !(list [0].Parent.Parent is MethodDeclarationSyntax) )
+                    return base.VisitList ( list );
+
+                return list
+                        .Insert ( 0, _PrologInvocationStatement as TNode )
+                        .Add ( _EpilogInvocationStatement as TNode );
+            }
+            public override SyntaxNode VisitMethodDeclaration ( MethodDeclarationSyntax node )
+            {
+                if ( node.Identifier.ValueText == "Main" )
+                    return base.VisitMethodDeclaration ( node );
+
+                if ( node.Identifier.ValueText == "Pippo" )
+                    return base.VisitMethodDeclaration ( node );
+
+                if ( node.Identifier.ValueText == "Pluto" )
+                    return base.VisitMethodDeclaration ( node );
+
+                if ( node.Identifier.ValueText == "Paperino" )
+                    return base.VisitMethodDeclaration ( node );
+
+                return node;
+            }
+
+
+            private InvocationExpressionSyntax _EpilogInvocationSyntax
+            {
+                get => SyntaxFactory.InvocationExpression (
+                        SyntaxFactory.IdentifierName ( "_Epilog" ) );
+            }
+
+            private ExpressionStatementSyntax _EpilogInvocationStatement
+            { get => SyntaxFactory.ExpressionStatement ( _EpilogInvocationSyntax ); }
+
+            private InvocationExpressionSyntax _ProogInvocationSyntax
+            {
+                get => SyntaxFactory.InvocationExpression (
+                        SyntaxFactory.IdentifierName ( "_Prolog" ) );
+            }
+
+            private ExpressionStatementSyntax _PrologInvocationStatement
+            { get => SyntaxFactory.ExpressionStatement ( _ProogInvocationSyntax ); }
+
+    }
+}
